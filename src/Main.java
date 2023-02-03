d1a247a4018358ed7f8c92edf5cf18c5cb88417e
freelkee <freelkee@ya.ru>
2023-02-03 19:19:02 +0300

    new_try

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int k = scanner.nextInt();
@@ -12,6 +13,58 @@ public class Main {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
+
+        Integer[] arrSorted = a.clone();
+        Arrays.sort(arrSorted);
+
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> aSort = new ArrayList<>(Arrays.asList(arrSorted));
+            int kCount = k;
+            Integer aI = a[i];
+            int dist = 0;
+            int indexOnSorted = aSort.indexOf(aI);
+            while (kCount > 0) {
+                int left;
+                int right;
+                try {
+                    left = aSort.get(indexOnSorted - 1);
+                } catch (Exception e) {
+                    right = aSort.get(indexOnSorted + 1);
+                    dist += Math.abs(aI - right);
+                    aSort.remove(indexOnSorted + 1);
+                    kCount -= 1;
+                    continue;
+                }
+                try {
+                    right = aSort.get(indexOnSorted + 1);
+                } catch (Exception e) {
+                    left = aSort.get(indexOnSorted - 1);
+                    dist += Math.abs(aI - left);
+                    aSort.remove(indexOnSorted - 1);
+                    indexOnSorted -= 1;
+                    kCount -= 1;
+                    continue;
+                }
+                if (Math.abs(aI - left) > Math.abs(aI - right)) {
+                    dist += Math.abs(aI - right);
+                    aSort.remove(indexOnSorted + 1);
+                } else if (Math.abs(aI - left) < Math.abs(aI - right)) {
+                    dist += Math.abs(aI - left);
+                    aSort.remove(indexOnSorted - 1);
+                    indexOnSorted -= 1;
+                } else if (Math.abs(aI - left) == Math.abs(aI - right)) {
+                    dist += Math.abs(aI - right);
+                    aSort.remove(indexOnSorted + 1);
+                }
+
+                kCount -= 1;
+            }
+            System.out.print(dist + " ");
+        }
+
+        /*
         for (int i = 0; i < n; i++) {
 
             ArrayList<Integer> dist = new ArrayList<>();
@@ -28,6 +81,7 @@ public class Main {
                 dist.remove((Integer) min);
             }
             System.out.print(realDist + " ");
-        }
+
+         */
     }
 }
